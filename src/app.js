fba67a547f5b549bf56634b5707c504328e7b08d
perminovea <87027883+perminovea@users.noreply.github.com>
2024-08-17 20:19:49 +0500

    add func lesson obj

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const scores = {
+        Anna: 10,
+        Olga: 1,
+        Ivan: 5,
+    }
+    /**
+     * Возвращает в ответ сумму всех баллов
+     *
+     * @param {*} data 
+     * @return {*} 
+     */
+function getScore(data) {
+    let listPerson = Object.keys(data);
+    let sAll = 0;
+    listPerson.forEach(name => sAll = sAll + data[name]);
+    console.log(sAll);
+    return sAll;
+}
+getScore(scores); // 16
