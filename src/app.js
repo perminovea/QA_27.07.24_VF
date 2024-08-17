ee9b0e12fd95aa055b391f9d87131f17d862670f
perminovea <87027883+perminovea@users.noreply.github.com>
2024-08-17 20:56:56 +0500

    old lesson

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const scores = {
-        Anna: 10,
-        Olga: 1,
-        Ivan: 5,
-    }
-    /**
-     * Возвращает в ответ сумму всех баллов
-     *
-     * @param {*} data 
-     * @return {*} 
-     */
+    Anna: 10,
+    Olga: 1,
+    Ivan: 5,
+};
+/**
+ * Возвращает в ответ сумму всех баллов
+ *
+ * @param {*} data F
+ * @return {*} 
+ */
 function getScore(data) {
     let listPerson = Object.keys(data);
     let sAll = 0;
@@ -16,4 +16,4 @@ function getScore(data) {
     console.log(sAll);
     return sAll;
 }
-getScore(scores); // 16
+getScore(scores);
